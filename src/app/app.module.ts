9ea2055828885c643391cd552ccbd594980682d2
jp <jpc.araujo@outlook,com>
2017-11-05 22:34:01 -0300

    Atualização do Bootstrap para versão beta, remoção da lib. tether, adição da lib. popper.js e adição do componente ordemCompra.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { OfertaComponent } from './oferta/oferta.component';
 import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
 import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
 import {DescricaoReduzida} from "./util/descricao-reduzida.pipe";
+import { OrdemCompraComponent } from './ordem-compra/ordem-compra.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import {DescricaoReduzida} from "./util/descricao-reduzida.pipe";
     OfertaComponent,
     ComoUsarComponent,
     OndeFicaComponent,
-    DescricaoReduzida
+    DescricaoReduzida,
+    OrdemCompraComponent
   ],
   imports: [
     BrowserModule,
