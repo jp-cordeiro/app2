8029b29f0bcd1a7b545d9390543612b20956c087
jp <jpc.araujo@outlook,com>
2017-11-11 12:30:43 -0300

    Exibição de mensagens de suporte na validação.

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -8,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class OrdemCompraComponent implements OnInit {
 
   public endereco: string = ''
-  public numero: number
+  public numero: string = ''
   public complemento: string = ''
   public formaPagamento: string = 'dinheiro'
+
+  //Controles de valicao dos campos
+  public enderecoValido: boolean
+  public numeroValido: boolean
+  public complementoValido: boolean
+  public formaPagamentoValido: boolean
+
+  //Estados primitivos dos campos (pristine)
+  //Controles de valicao dos campos
+  public enderecoEstadoPrimitivo: boolean = true
+  public numeroEstadoPrimitivo: boolean = true
+  public complementoEstadoPrimitivo: boolean = true
+  public formaPagamentoEstadoPrimitivo: boolean = true
 
   constructor() { }
 
@@ -18,19 +31,52 @@ export class OrdemCompraComponent implements OnInit {
   }
 
 
-  atualizarEndereco(endereco: string): void{
+  atualizaEndereco(endereco: string): void{
     this.endereco = endereco
+
+    this.enderecoEstadoPrimitivo = false
+
+    if(this.endereco.length > 3){
+      this.enderecoValido = true
+    }
+    else {
+      this.enderecoValido = false
+    }
   }
 
-  atualizarNumero(numero: number): void{
+  atualizaNumero(numero: string): void{
     this.numero = numero
+
+    this.numeroEstadoPrimitivo = false
+
+    if(this.numero.length > 0){
+      this.numeroValido = true
+    }
+    else {
+      this.numeroValido = false
+    }
   }
 
-  atualizarComplemento(complemento: string): void{
+  atualizaComplemento(complemento: string): void{
     this.complemento = complemento
+
+    this.complementoEstadoPrimitivo = false
+
+    if(this.complemento.length > 3){
+      this.complementoValido = true
+    }
   }
 
-  atualizarFormaPagamento(formaPagamento: string): void{
+  atualizaFormaPagamento(formaPagamento: string): void{
     this.formaPagamento = formaPagamento
+
+    this.formaPagamentoEstadoPrimitivo = false
+
+    if(this.formaPagamento.length > 0){
+      this.formaPagamentoValido = true
+    }
+    else {
+      this.formaPagamentoValido = false
+    }
   }
 }
