eea97546d276670b994967c6e0c838460946e1ea
jp <jpc.araujo@outlook,com>
2017-11-11 10:45:05 -0300

    Two-Way-Binding dos dados do formulário de Ordem de Compra

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public endereco: string = ''
+  public numero: number
+  public complemento: string = ''
+  public formaPagamento: string = 'dinheiro'
+
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  atualizarEndereco(endereco: string): void{
+    this.endereco = endereco
+  }
+
+  atualizarNumero(numero: number): void{
+    this.numero = numero
+  }
+
+  atualizarComplemento(complemento: string): void{
+    this.complemento = complemento
+  }
+
+  atualizarFormaPagamento(formaPagamento: string): void{
+    this.formaPagamento = formaPagamento
+  }
 }
